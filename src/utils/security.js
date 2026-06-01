151ce4c20f214db8384adae988edb74aa5c7b5f5
AsadbekFullstack <toirovasadbek1706@gmail.com>
2026-06-02 03:08:42 +0500

    asdasd

--- src/utils/security.js
+++ src/utils/security.js
@@ -1,15 +1,7 @@
 export const secureApp = () => {
   if (import.meta.env.PROD) {
-    ["log", "warn", "debug", "info", "error"].forEach((m) => {
+    ["log", "debug", "info"].forEach((m) => {
       console[m] = () => {};
-    });
-  }
-
-  if (import.meta.env.PROD && typeof window !== "undefined") {
-    Object.defineProperty(window, "console", {
-      value: {},
-      writable: false,
-      configurable: false,
     });
   }
 };
